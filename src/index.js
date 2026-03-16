53fb9f4e5343864cdd53041700940ec43251338d
Daria Seleznova <dariaseleznova1@gmail.com>
2026-03-16 19:00:20 +0100

    add header+media & hero+media

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 require('./style.scss');
+
+const burger = document.getElementById("burger-menu");
+const closeBtn = document.getElementById("closeBtn");
+const nav = document.querySelector(".menu");
+
+burger.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    nav.classList.remove("active");
+});
+const searchBtn = document.getElementById("searchBtn");
+const mobileBtns = document.querySelector(".mobile-btn");
+
+searchBtn.addEventListener("click", () => {
+    mobileBtns.classList.toggle("search-active");
+});
+
+document.addEventListener("click", (e) => {
+
+    if (!mobileBtns.contains(e.target)) {
+        mobileBtns.classList.remove("search-active");
+    }
+
+    if (!nav.contains(e.target) && !burger.contains(e.target)) {
+        nav.classList.remove("active");
+    }
+
+});
